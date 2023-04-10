4bb01bd7e061763b6cb8929c4310cb86ef281009
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-04-10 11:48:19 +0900

    23-4-6: last shoot

--- 23-4-6/CJ/2_3190/main.cpp
+++ 23-4-6/CJ/2_3190/main.cpp
@@ -25,7 +25,7 @@ bool isHeadOK() {
     return true;
 }
 
-void printBoard() {
+void printBoard(int time) {
     cout << time << " " << head.Y << " " << head.X << " "
          << "\n";
     for (int i = 1; i <= board_size; i++) {

8a721885b435b2a80698c576d3dab33dfdd053ba
Henry-Hong <hhr8540@naver.com>
2024-03-09 16:35:42 +0900

    file move

--- 24-1/HONG/24-1-23/dijkstra.py
+++ 24-1/HONG/24-1-23/dijkstra.py
@@ -1,31 +1,31 @@
 import heapq
 
 def dijkstra(graph, start):
-    distances = {node: float('infinity') for node in graph}
-    distances[start] = 0
+  distances = {node: float('infinity') for node in graph}
+  distances[start] = 0
 
-    priority_queue = [(0, start)]
+  priority_queue = [(0, start)]
 
-    while priority_queue:
-        current_distance, current_node = heapq.heappop(priority_queue)
+  while priority_queue:
+    current_distance, current_node = heapq.heappop(priority_queue)
 
-        # 업데이트된 최단 거리 출력
-        print(f"Update distance to node {current_node}: {current_distance}")
+    # 업데이트된 최단 거리 출력
+    print(f"Update distance to node {current_node}: {current_distance}")
 
-        if current_distance > distances[current_node]:
-            continue
+    if current_distance > distances[current_node]:
+      continue
 
-        for neighbor, weight in graph[current_node].items():
-            distance = current_distance + weight
+    for neighbor, weight in graph[current_node].items():
+      distance = current_distance + weight
 
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(priority_queue, (distance, neighbor))
+      if distance < distances[neighbor]:
+        distances[neighbor] = distance
+        heapq.heappush(priority_queue, (distance, neighbor))
 
-        # 업데이트된 distances 출력
-        print("Updated distances:", distances)
+      # 업데이트된 distances 출력
+      print("Updated distances:", distances)
 
-    return distances
+  return distances
 
 # 예시 그래프
 example_graph = {
